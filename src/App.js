3eb80ca2b3b5f3f4411b91ae0108de6b659e2893
FOX-P1 <smgms7513@gmail.com>
2021-07-29 15:27:12 +0900

    react_again_5

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import React, {
 // import InputSample from "./inputSample";
 import UserList from "./UserList";
 import CreateUser from "./createUser";
-import useInputs from "./hooks/useInputs";
+import produce from "immer";
 
 function countActionUsers(users) {
     console.log("활성 사용자 수를 세는중...");
@@ -51,33 +51,22 @@ const initialState = {
 
 function reducer(state, action) {
     switch (action.type) {
-        case "CHANGE_INPUT":
-            return {
-                ...state,
-                inputs: {
-                    ...state.inputs,
-                    [action.name]: action.value,
-                },
-            };
         case "CREATE_USER":
-            return {
-                inputs: initialState.inputs,
-                users: state.users.concat(action.user),
-            };
+            return produce(state, (draft) => {
+                draft.users.push(action.user);
+            });
         case "TOGGLE_USER":
-            return {
-                ...state,
-                users: state.users.map((user) =>
-                    user.id === action.id
-                        ? { ...user, active: !user.active }
-                        : user
-                ),
-            };
+            return produce(state, (draft) => {
+                const user = draft.users.find((user) => user.id === action.id);
+                user.active = !user.active;
+            });
         case "REMOVE_USER":
-            return {
-                ...state,
-                users: state.users.filter((user) => user.id !== action.id),
-            };
+            return produce(state, (draft) => {
+                const index = draft.users.findIndex(
+                    (user) => user.id === action.id
+                );
+                draft.users.splice(index, 1);
+            });
         default:
             return state;
     }
@@ -87,12 +76,7 @@ function reducer(state, action) {
 export const UserDispatch = React.createContext(null);
 
 function App() {
-    const [{ username, email }, onChange, reset] = useInputs({
-        username: "",
-        email: " ",
-    });
     const [state, dispatch] = useReducer(reducer, initialState);
-    const nextId = useRef(4);
 
     const { users } = state;
 
@@ -107,46 +91,14 @@ function App() {
     //     });
     // }, []);
 
-    const onCreate = useCallback(() => {
-        dispatch({
-            type: "CREATE_USER",
-            user: {
-                id: nextId.current,
-                username,
-                email,
-            },
-        });
-        reset();
-        nextId.current += 1;
-    }, [username, email, reset]);
-
-    const onToggle = useCallback((id) => {
-        dispatch({
-            type: "TOGGLE_USER",
-            id,
-        });
-    }, []);
-
-    const onRemove = useCallback((id) => {
-        dispatch({
-            type: "REMOVE_USER",
-            id,
-        });
-    }, []);
-
     const count = useMemo(() => countActionUsers(users), [users]);
 
     return (
-        <UserDispatch>
-            <CreateUser
-                username={username}
-                email={email}
-                onChange={onChange}
-                onCreate={onCreate}
-            />
-            <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+        <UserDispatch.Provider value={dispatch}>
+            <CreateUser />
+            <UserList users={users} />
             <div>활성사용자 수 : {count}</div>
-        </UserDispatch>
+        </UserDispatch.Provider>
         // <div>
         //     <InputSample show={show} />
         //     <button onClick={showSample}>Show</button>
