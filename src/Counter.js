c4049cbf361adc3a2cd22980f374c2371d48225b
FOX-P1 <smgms7513@gmail.com>
2021-07-29 17:51:54 +0900

    react_again_6

--- src/Counter.js
+++ src/Counter.js
@@ -1,66 +1,37 @@
-import React, { useReducer } from "react";
+import React, { Component } from "react";
 
-function reducer(state, action) {
-    switch (action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-        default:
-            return state;
+class Counter extends Component {
+    state = {
+        counter: 0,
+        fixed: 1,
+    };
+    handleIncrease = () => {
+        this.setState(
+            {
+                counter: this.state.counter + 1,
+            },
+            () => {
+                console.log(this.state.counter);
+            }
+        );
+    };
+
+    handleDecrease = () => {
+        this.setState((state) => ({
+            counter: this.state.counter - 1,
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.handleIncrease}>+1</button>
+                <button onClick={this.handleDecrease}>-1</button>
+                <p>고정된 값: {this.state.fixed}</p>
+            </div>
+        );
     }
 }
 
-function Counter() {
-    const [number, dispatch] = useReducer(reducer, 0);
-
-    const onIncrease = () => {
-        dispatch({ type: "INCREMENT" });
-    };
-    const onDecrease = () => {
-        dispatch({ type: "DECREMENT" });
-    };
-
-    return (
-        <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
-        </div>
-    );
-}
-
 export default Counter;
-
-// import React, { useState } from "react";
-
-// function Counter() {
-//     const [changeNum, setChangeNum] = useState(0);
-//     const [number, setNumber] = useState(0);
-
-//     const onIncrease = () => {
-//         console.log(typeof number);
-//         console.log(typeof changeNum);
-//         setNumber((prevNumber) => prevNumber + changeNum);
-//     };
-//     const onDecrease = () => {
-//         console.log(typeof number);
-//         console.log(typeof changeNum);
-//         setNumber((prevNumber) => prevNumber - changeNum);
-//     };
-
-//     const onChange = (event) => {
-//         setChangeNum(parseInt(event.target.value, 10));
-//     };
-
-//     return (
-//         <div>
-//             <h1>{number}</h1>
-//             <input type="number" onChange={onChange} value={changeNum} />
-//             <button onClick={onIncrease}>+</button>
-//             <button onClick={onDecrease}>-</button>
-//         </div>
-//     );
-// }
-
-// export default Counter;
